172ebe511d8ab6caa3c529cb3e71dab212d4406f
juliandito <vjuliandito@gmail.com>
2020-11-14 23:34:55 +0700

    tambah while

--- no8_search.py
+++ no8_search.py
@@ -1,10 +1,12 @@
 def cari(a, v):
-    try:
-        index = a.index(v)
-    except:
-        index = -1
-    
-    return index
+    i = 0
+    alamat = -1
+    while i < len(a):
+        if a[i] == v:
+            alamat=i
+        i += 1
+
+    return alamat
     
 from no4_random import rand
 #bikin list pake fungsi nomer 4
